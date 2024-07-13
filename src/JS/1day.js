ce79ec1cb2690c99c940411b02be1c7a167f1d45
MD Talib Ansari <talib.ansari@swanbay.tv>
2024-07-13 16:21:25 +0530

    first day challenge completed

---
+++ src/JS/1day.js
@@ -0,0 +1,46 @@
+// Day 1 Variables and Data Types
+
+
+// ACTIVITY - 1 Variables Declaration.
+// Task:1
+var firstVar = 10;
+console.log("This is my first variable using 'var' in this challenge: ", firstVar);
+
+// Task:2
+let firstLet = 20;
+console.log("This is my first variable using 'let' in this challenge: ", firstLet);
+
+// ACTIVITY - 2 Constant Declaration.
+// Task:3
+const firstConst = 30;
+console.log("This is my first variable using 'const' in this challenge: ", firstConst);
+
+// ACTIVITY - 3 Data Types.
+// Task:4
+const number = 30;
+const string = '30';
+const boolean = true;
+const object = { id: 30, name: 'MD Talib Ansari', position: 'MEAN Stack Developer.' };
+const array = [30, 'string', true, { id: 1, department: "Developer" }];
+
+console.log("Data Type of Number: ", typeof number);
+console.log("Data Type of string: ", typeof string);
+console.log("Data Type of boolean: ", typeof boolean);
+console.log("Data Type of object: ", typeof object);
+console.log("Data Type of array: ", typeof array);
+
+// ACTIVITY - 4 Reassigning Variables.
+// Task:5 
+let letReassign = 30;
+letReassign = 40;
+
+console.log("Let Reassign Value: ", letReassign);
+
+// ACTIVITY - 5 Understanding Const.
+// Task:6
+const constReassign = 50;
+constReassign = 60;
+
+console.log("const Reassign Value: ", constReassign);
+
+
