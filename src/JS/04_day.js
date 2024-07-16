fb7528ac609a44199ba8f5c980046a05e09b23ba
MD Talib Ansari <mdtalib.ansari@fluper.in>
2024-07-16 10:00:46 +0530

    Fourth day challenge complete

--- src/JS/04_day.js
+++ src/JS/04_day.js
@@ -62,3 +62,20 @@ for (let i = 0; i < 5; i++) {
 }
 console.log(star);
 
+
+// ACTIVITY - 4 Loop Control Statements.
+// Task:8 write a program to print numbers from 1 to 10, but skip the number 5 using continue statement.
+for (let i = 1; i <= 10; i++) {
+    if (i === 5) {
+        continue;
+    }
+    console.log(i);
+}
+
+// Task:9 write a program to print numbers from 1 to 10, but stop the loop when the number is 7 using the break statement.
+for (let i = 1; i <= 10; i++) {
+    if (i === 7) {
+        break;
+    }
+    console.log(i);
+}
