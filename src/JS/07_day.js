5950c8be8d3621dca057e55e35091c968c809ff2
MD Talib Ansari <talibansari1510@gmail.com>
2024-07-19 08:26:32 +0530

    Seventh day challenge complete

--- src/JS/07_day.js
+++ src/JS/07_day.js
@@ -58,7 +58,11 @@ console.log(book.returnTitleAndYear());
 
 
 // ACTIVITY - 5 Object Iteration.
-// Task:8 Use a for...in loop to iterate over the properties of the book object and log each property and its value.
+// Task:8 Use a 'for...in' loop to iterate over the properties of the book object and log each property and its value.
 for (const key in book) {
     console.log(`The book property: ${key} and value: ${book[key]}`);
 }
+
+// Task:9 Use a 'Object.keys' and 'Object.values' methods to log all the keys and values of the book object.
+console.log(`All keys: `,Object.keys(book));
+console.log(`All values: `,Object.values(library));
