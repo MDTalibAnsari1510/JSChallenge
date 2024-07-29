b8aeb991bf55da4b85db886f21a2110a0fb0b7be
MD Talib Ansari <talibansari1510@gmail.com>
2024-07-29 08:30:03 +0530

    Seventeenth day challenge start

---
+++ src/JS/DSA/17_day.js
@@ -0,0 +1,34 @@
+// Day 17: Data Structures
+
+// ACTIVITY - 1 Linked List
+// Task:1 Implement Node class to represent an element in a linked list with properties value and next.
+
+// Task:2 Implement LinkedList class with methods to add a node to the end, remove a node from the end, and display all nodes.
+
+
+
+// ACTIVITY - 2 Stack
+// Task:3 Implement stack class with methods push(add element) pop(remove element) and peek(view the top element).
+
+// Task:4 Use the Stack class to reverse a string by pushing all character onto the stack and then popping them off.
+
+
+
+// ACTIVITY - 3 Queue
+// Task:5 Implement Queue class with methods enqueue(add element), dequeue(remove element), and front(view element).
+
+// Task:6 Use the Queue class to simulate a simple printer queue where print jobs are added ti the queue and processed in order.
+
+
+
+// ACTIVITY - 4 Binary Tree
+// Task:7 Implement a TreeNode class to represent a node in a binary tree with properties value, left, and right.
+
+// Task:8 Implement a BinaryTree class with methods for inserting values and performing in-order traversal to display nodes.
+
+
+
+// ACTIVITY - 5 Graph (Optional)
+// Task:9 Implement a Graph class with methods to add vertices, add edges, and perform a breadth-first search (BSF).
+
+// Task:10 Use the Graph class to represent a simple network and perform (BSF) to find the shortest path between two nodes.
