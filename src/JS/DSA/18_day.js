3fe6bb5b5a79bfb1c09cfebb91a5048c330547e0
MD Talib Ansari <talibansari1510@gmail.com>
2024-08-04 09:05:38 +0530

    Eighteenth day challenge start

--- src/JS/DSA/18_day.js
+++ src/JS/DSA/18_day.js
@@ -1,4 +1,26 @@
 // Day 18: Algorithms
 
 // ACTIVITY - 1 Sorting Algorithms
-// Task:1 Implement 
+// Task:1 Implement the bubble sort algorithm to sort an array of numbers in ascending order. Log the sorted array.
+// Task:2 Implement the selection sort algorithm to sort an array of numbers in ascending order. Log the sorted array.
+// Task:3 Implement the quicksort algorithm to sort an array of numbers in ascending order. Log the sorted array.
+
+
+// ACTIVITY - 2 Searching Algorithms
+// Task:4 Implement the linear seach algorithm to find a target value in an array. Log the the index of the target value.
+// Task:5 Implement the binary seach algorithm to find a target value in n sorted array. Log the the index of the target value.
+
+
+// ACTIVITY - 3 String Algorithms
+// Task:6 Wriet a function to count the occurrences of each character in a string. Log the character counts.
+// Task:7 Wriet a function to find the longest substring without repeating characters in a string count. Log the length of the sunstring.
+
+
+// ACTIVITY - 4 Array Algorithms
+// Task:8 Wriet a function to rotate an array by k place position. Log the rotated array.
+// Task:9 Wriet a function to merge two sorted arrays into a one sorted array. Log the merged array.
+
+
+// ACTIVITY - 5 Dynamic Programming (Optional)
+// Task:10 Wriet a function to solve the fibonacci sequence using dynamic programming. Log the fibonacci numbers.
+// Task:11 Wriet a function to solve the knapsack problem using dynamic programming. Log the maximum value that can be obtained.
