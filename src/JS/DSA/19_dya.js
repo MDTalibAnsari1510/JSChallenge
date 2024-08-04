1bedb09f191186ecc866cafc05e6a7fd6b7956cc
MD Talib Ansari <talibansari1510@gmail.com>
2024-08-04 09:30:35 +0530

    Nineteenth day challenge start

---
+++ src/JS/DSA/19_dya.js
@@ -0,0 +1,27 @@
+// Day 19: Regular Expressions
+
+// ACTIVITY - 1 Basic Regular Expressions
+// Task:1 Write a regular expression to match a simple pattern (e.g, match all occurances of word "JavaScript" in a string). Log the matches.
+// Task:2 Write a regular expression to match all digits in a string. Log the matches.
+
+
+// ACTIVITY - 2 Character Classes and Quantifiers
+// Task:3 Write a regular expression to match all words in a string that start with a capital letter. Log the matches.
+// Task:4 Write a regular expression to match all sequence of one or more digits in a string that. Log the matches.
+
+
+// ACTIVITY - 3 Grouping And Capturing
+// Task:5 Write a regular expression to capture the area code, central office, and line number from US phone number format (e.g, (123) 456-7890). Log the captures.
+// Task:6 Write a regular expression to capture the username and domain from an email address. Log the captures.
+
+
+// ACTIVITY - 4 Assertions and Boundaries
+// Task:7 Write a regular expression to match a word only if it is at the beginning of a string. Log the matches.
+// Task:8 Write a regular expression to match a word only if it is at the end of a string. Log the matches.
+
+
+// ACTIVITY - 5 Practical Applications
+// Task:9 Write a regular expression to validate a simple password (must includes at least one Uppercase letter, one lowercase letter, one digit, and one special character). Log whether the password vaild.
+// Task:10 Write a regular expression to validate a URL. Log whether the URL is vaild.
+
+
