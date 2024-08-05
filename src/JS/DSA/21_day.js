2d8dee6e04183b55508ef37f013c68f6635819bc
MD Talib Ansari <talibansari1510@gmail.com>
2024-08-05 08:18:47 +0530

    Twintyfirst day challenge start

---
+++ src/JS/DSA/21_day.js
@@ -0,0 +1,33 @@
+// Day 21: LeetCode Easy
+
+// ACTIVITY - 1 Two Sum
+// Task:1 Solve the "Two Sum" Problem on LeetCode.
+//      Write a function that takes an array of numbers and a target number, and returns the indices of the two numbers that add up to the target.
+//      Log the indices for a few test cases.
+
+
+
+// ACTIVITY - 2 Reverse Integer
+// Task:2 Solve the "Reverse Integer" Problem on LeetCode.
+//      Write a function that takes an integer and returns it with its digit reversed.
+//      Handle edge cases like negative numbers and numbers ending in zero.
+//      Log the reversed integer for a few test cases.
+
+
+// ACTIVITY - 3 Palindrome Number
+// Task:3 Solve the "Palindrome Number" Problem on LeetCode.
+//      Write a function that takes an integer and returns true if it is palindrome, and false otherwise.
+//      Log the result for a few test cases, including edge cases like nagative numbers.
+
+
+// ACTIVITY - 4 Merge Two Sorted Lists
+// Task:4 Solve the "Merge Two Sorted Lists" Problem on LeetCode.
+//      Write a function that takes two sorted linked lists and returns a new sorted list by merging them.
+//      Create a few test cases, with linked lists and log the merge list.
+
+
+// ACTIVITY - 5 Valid Parantheses
+// Task:4 Solve the "Valid Parantheses" Problem on LeetCode.
+//      Write a function that takes string containing just the characters '(', ')', '{','}','[', and ']', and determines if the input sting is valid.
+//      A string is valid if open brackets are closed in correct order.
+//      Log the result for a few test cases.
