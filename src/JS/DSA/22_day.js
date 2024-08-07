3444627f2891094e01561b5c7604ac8716d0db24
MD Talib Ansari <talibansari1510@gmail.com>
2024-08-07 23:22:11 +0530

    Twintysecond day challenge start

---
+++ src/JS/DSA/22_day.js
@@ -0,0 +1,30 @@
+// Day 21: LeetCode Easy
+
+// ACTIVITY - 1 Add Two Numbers 
+// Task:1 Solve the "Add Two Numbers" Problem on LeetCode.
+//      Write a function that takes two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each node contains a single digit. Add the two numbers and returns the sum as a linked list.
+//      Create a few test cases with linked lists and log the sum as linked list.
+
+
+// ACTIVITY - 2 Longest Substring Without Repeating Characters 
+// Task:2 Solve the "Longest Substring Without Repeating Characters" Problem on LeetCode.
+//      Write a function that takes a string and returns the length of the longest substring without repeating characters.
+//      Log the length for a few test cases, including edge cases.
+
+
+// ACTIVITY - 3 Container With Most water
+// Task:3 Solve the "Container With Most water" Problem on LeetCode.
+//      Write a function that takes an array of non-integers where each integer represents the height of the line drawn at each point. Find Two lines that together with the x-axis from a container, such that the container holds the most water.
+//      Log the maximum amount of water for a few test cases.
+
+
+// ACTIVITY - 4 3Sum
+// Task:4 Solve the "3Sum" Problem on LeetCode.
+//      Write a function that takes an array of integers and finds all unique triplets in the array which give the sum of zero.
+//      Log the triplets for a few test cases, includung edge cases.
+
+
+// ACTIVITY - 5 Group Anagrams
+// Task:5 Solve the "Group Anagrams" Problem on LeetCode.
+//      Write a function that takes an array of string and groups anagrams together.
+//      Log the grouped anagrams for a few test cases.
